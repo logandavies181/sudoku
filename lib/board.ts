70809ee5bc9665910fd30f3cb0114d1e6668f552
logandavies181 <logan.davies181@gmail.com>
2025-03-07 21:52:49 +1300

    fmt

--- lib/board.ts
+++ lib/board.ts
@@ -2,7 +2,7 @@ import { html } from "../html.ts"
 
 export function Board() {
   return html`
-    <div class="flex grow flex-wrap bg-white min-w-full" >
+    <div class="flex grow flex-wrap bg-white min-w-full">
       <${Box} />
       <${Box} />
       <${Box} />
@@ -18,7 +18,7 @@ export function Board() {
 
 function Box() {
   return html`
-    <div class="border-2 border-solid flex grow flex-wrap bg-white max-w-1/3 min-w-1/3" >
+    <div class="border-2 border-solid flex grow flex-wrap bg-white max-w-1/3 min-w-1/3">
       <${Cell} />
       <${Cell} />
       <${Cell} />
@@ -34,8 +34,8 @@ function Box() {
 
 function Cell() {
   return html`
-    <div class="text-md border-1 border-solid flex text-center justify-center items-center aspect-square min-w-1/3" >
-      ${Math.random() * 10 % 10 | 1}
+    <div class="text-md border-1 border-solid flex text-center justify-center items-center aspect-square min-w-1/3">
+      ${(Math.random() * 10) % 10 | 1}
     </div>
   `
 }
