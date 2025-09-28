0d4114d31d59ea755d025073e0c3f050355cd709
logandavies181 <logan.davies181@gmail.com>
2025-09-28 16:15:17 +1300

    make stack usable and actually modify value on push/pop

--- pkg/stack/solve_stack.go
+++ pkg/stack/solve_stack.go
@@ -1,7 +1,5 @@
 package stack
 
-var _solveStack solveStack
-
 type SolveStackItem struct {
 	Index int
 	Value int
@@ -10,20 +8,20 @@ type SolveStackItem struct {
 
 type solveStack []SolveStackItem
 
-func (s solveStack) Push(item SolveStackItem) {
-	s = append(s, item)
+func (s *solveStack) Push(item SolveStackItem) {
+	*s = append(*s, item)
 }
 
-func (s solveStack) Pop() SolveStackItem {
-	ret := s[len(s)-1]
-	s = s[:len(s)-1]
-	return ret
+func (s *solveStack) Pop() *SolveStackItem {
+	if len(*s) == 0 {
+		return nil
+	}
+
+	ret := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return &ret
 }
 
-func Push(item SolveStackItem) {
-	_solveStack.Push(item)
-}
-
-func Pop() SolveStackItem {
-	return _solveStack.Pop()
+func New() solveStack {
+	return make(solveStack, 0)
 }
